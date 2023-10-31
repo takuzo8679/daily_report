c171b4515e371dea8504d6714b9b9d52ee42dd91
takuzo <takuya.koiso.s@gmail.com>
2023-11-01 04:07:25 +0900

    fix: mkdir if not exists

--- 2023/cprepo.sh
+++ 2023/cprepo.sh
@@ -1,4 +1,11 @@
 TODAY=$(date "+%m/%d").md #mm/dd.md
 YESTERDAY=$(date -v -1d "+%m/%d").md
+
+# mkdir if not exits
+DIR_NAME=$(date "+%m")
+if [ ! -d $DIR_NAME ]; then
+  mkdir $DIR_NAME
+fi
+
 cp $YESTERDAY $TODAY
 code $TODAY # open file with vscode
