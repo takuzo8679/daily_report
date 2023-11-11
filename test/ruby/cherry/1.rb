3b850da21c48238a54e989472aeed81c9112a74e
takuzo <takuya.koiso.s@gmail.com>
2023-11-11 15:01:30 +0900

    rubocop

--- test/ruby/cherry/1.rb
+++ test/ruby/cherry/1.rb
@@ -1,9 +1,13 @@
+# frozen_string_literal: true
+
 require 'debug'
+# femaleのハッシュのみ取得
 [
-  {name: 'Alice', age: 11, gender: 'female'},
-  {name: 'Bob',   age: 12, gender: 'male'},
-  {name: 'Carol', age: 13, gender: 'female'},
-].select{|p| p in {gender: 'afemale'}
-  } => females # femaleのハッシュのみ取得
-  binding.break
+  { name: 'Alice', age: 11, gender: 'female' },
+  { name: 'Bob', age: 12, gender: 'male' },
+  { name: 'Carol', age: 13, gender: 'female' }
+].select do |p|
+  p in { gender: 'female' }
+end => females
+
 puts females
