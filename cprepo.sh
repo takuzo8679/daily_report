ead534235e609ab9228238d333d51cc67bc8fdd2
takuzo <takuya.koiso.s@gmail.com>
2024-01-04 05:15:33 +0900

    2024/01/03

--- cprepo.sh
+++ cprepo.sh
@@ -2,7 +2,7 @@ TODAY=$(date "+%Y/%m/%d").md #yyyy/mm/dd.md
 YESTERDAY=$(date -v -1d "+%Y/%m/%d").md
 
 # mkdir if not exits
-DIR_NAME=$(date "+%m")
+DIR_NAME=$(date "+%Y/%m")
 if [ ! -d $DIR_NAME ]; then
   mkdir -p $DIR_NAME
 fi
